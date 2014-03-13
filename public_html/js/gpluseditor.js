e548f3e908ab53d73c3b6cdc85160137e8b5c6f0
Paul Spoerry <Paul@PaulSpoerry.com>
2014-03-13 15:58:38 -0500

    Update 1.0.3 resolve postback
    Stop extension for letting href cause post which led to blank screen.

--- public_html/js/gpluseditor.js
+++ public_html/js/gpluseditor.js
@@ -42,7 +42,10 @@ chrome.extension.sendMessage({}, function(response) {
             // Launch TipTip tooltip
             $('.tiptip a.button, .tiptip button').tipTip();
  
-            $( "#homehomeontheweb" ).click(function() { window.open("http://www.paulspoerry.com/code/post-editor-for-google-plus/"); });
+            $( "#homehomeontheweb" ).click(function(e) { 
+                e.preventDefault();
+                window.open("http://www.paulspoerry.com/code/post-editor-for-google-plus/"); 
+            });
             $( "#gpeBold" ).mousedown(function() { applyPlusStyle("bold"); });
             $( "#gpeItalic" ).mousedown(function() { applyPlusStyle("italic"); });
             $( "#gpeStrike" ).mousedown(function() { applyPlusStyle("strike"); });
